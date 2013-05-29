94f761e71a2dcdf4e08ca608eca9a879618bd3db
Allen Lee <allenwlee@Allens-MacBook-Pro.local>
2013-05-28 21:41:39 -0700

    not done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,9 +9,34 @@ get '/events/:id/show' do |id|
 end
 
 get '/events/new' do
-  #TODO IMPLEMENT ME
+  erb :create_event
 end
 
+
 post '/events/create' do
-  #TODO IMPLEMENT ME
+  p params
+  @error_message = []
+  @event = Event.new(params[:event])
+
+  if Event.all.map { |e| e.title }.include?(@event.title)
+    @error_message << 'title already exists'
+  end
+  if @event.organizer_name.nil?
+    @error_message << 'you must enter an organizer name'
+  end
+
+  @error_message << 'invalid email' unless @event.organizer_email =~ /\w+@\w+.\w{2,}/
+    
+  if @event.organizer_email.nil?
+    @error_message << 'you must enter an organizer email'
+  end
+
+  if @error_message.length == 0
+    @event.save
+    redirect '/'
+  else
+    erb :error_message
+  end
+  
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,16 @@
 class Event < ActiveRecord::Base
+
+  validates :date, :presence => { :message => "you must enter in a date" }
+  validates :title, :uniqueness => { :message => "event title is already taken" }
+  validates :organizer_name, :presence => { :message => "you must enter in an organizer name" }
+  validates :organizer_email, :presence => { :message => "you must enter in an organizer email" }
+  validates_format_of :organizer_email, :with => /\w+@\w+.\w{2,}/
+  # validate :proper_date
+  
+
+
+  # def proper_date
+  #   self.date > Time.now
+  # end
+
 end
